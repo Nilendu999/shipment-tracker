a1f7faaad3194aaac2cd6eacce7b18e57ab4372a
Nilendu Ganguli <nilenduganguli999@gmail.com>
2022-02-21 20:45:49 +0530

    changed 4 column test script to final table format with 8 cols.

--- insertdata.php
+++ insertdata.php
@@ -1,6 +1,9 @@
 <?php
+    //CONNECTING TO DATABASE SERVER
     $conf = file_get_contents('dbconnect.conf');
     $config = json_decode($conf,true);
+    $tabcols = $config['table_cols'];
+    //print_r($tabcols);
     $conn=mysqli_connect($config['hostname'],$config['username'],$config['password'],$config['database']) or die("Connection Failed: " .mysqli_connect_error());
     /*$time = "1999-9-11 23:24:21";
     $temp = "11.6";
@@ -17,15 +20,21 @@
     else{
         echo("Entry failed");
     }*/
+    //CHECKING FOR POST REQUEST
     if($_SERVER['REQUEST_METHOD']=='POST'){
-        if(isset($_POST['time']) && isset($_POST['temp']) && isset($_POST['humidity']) && isset($_POST['jerk'])){
-            $time = $_POST['time'];
-            $temp = $_POST['temp'];
-            $humidity = $_POST['humidity'];
-            $jerk = $_POST['jerk'];
+        if(isset($_POST['datetime'] ,$_POST['latitude'] ,$_POST['longitude'] ,$_POST['temperature'] ,$_POST['humidity'] ,$_POST['acceleration_x'] ,$_POST['acceleration_y'] ,$_POST['acceleration_z'])){
+            $datetime = $_POST['datetime'];
+            $lat = $_POST['latitude'];
+            $lon = $_POST['longitude'];
+            $temp = $_POST['temperature'];
+            $hum = $_POST['humidity'];
+            $accx = $_POST['acceleration_x'];
+            $accy = $_POST['acceleration_y'];
+            $accz = $_POST['acceleration_z'];
             $table = $config['table'];
 
-            $sql_statement = "INSERT INTO $table(time,temp,humidity,jerk) VALUES ('$time','$temp','$humidity','$jerk')";
+            $sql_statement = "INSERT INTO $table($tabcols[0],$tabcols[1],$tabcols[2],$tabcols[3],$tabcols[4],$tabcols[5],$tabcols[6],$tabcols[7]) VALUES ('$datetime','$lat','$lon','$temp','$hum','$accx','$accy','$accz')";
+            #echo($sql_statement);
             $sqlquery = mysqli_query($conn,$sql_statement);
             if($sqlquery){
                 echo("1");
@@ -35,16 +44,22 @@
             }
         }
     }
-    //http://localhost/fyp/insertdata.php?time=2021-02-21+23:22:22&temp=12.6&humidity=13.5&jerk=2.0
+    //CHECKING FOR GET REQUESTS
+    //http://localhost/fyp/insertdata.php?datetime=2021-02-21+23:22:22&latitude=10.009&longitude=12.192&temperature=12.6&humidity=13.5&acceleration_x=0&acceleration_y=0&acceleration_z=9.81
     if($_SERVER['REQUEST_METHOD']=='GET'){
-        if(isset($_GET['time']) && isset($_GET['temp']) && isset($_GET['humidity']) && isset($_GET['jerk'])){
-            $time = $_GET['time'];
-            $temp = $_GET['temp'];
-            $humidity = $_GET['humidity'];
-            $jerk = $_GET['jerk'];
+        if(isset($_GET['datetime'] ,$_GET['latitude'] ,$_GET['longitude'] ,$_GET['temperature'] ,$_GET['humidity'] ,$_GET['acceleration_x'] ,$_GET['acceleration_y'] ,$_GET['acceleration_z'])){
+            $datetime = $_GET['datetime'];
+            $lat = $_GET['latitude'];
+            $lon = $_GET['longitude'];
+            $temp = $_GET['temperature'];
+            $hum = $_GET['humidity'];
+            $accx = $_GET['acceleration_x'];
+            $accy = $_GET['acceleration_y'];
+            $accz = $_GET['acceleration_z'];
             $table = $config['table'];
 
-            $sql_statement = "INSERT INTO $table(time,temp,humidity,jerk) VALUES ('$time','$temp','$humidity','$jerk')";
+            $sql_statement = "INSERT INTO $table($tabcols[0],$tabcols[1],$tabcols[2],$tabcols[3],$tabcols[4],$tabcols[5],$tabcols[6],$tabcols[7]) VALUES ('$datetime','$lat','$lon','$temp','$hum','$accx','$accy','$accz')";
+            echo($sql_statement);
             $sqlquery = mysqli_query($conn,$sql_statement);
             if($sqlquery){
                 echo("1");
--- test_requests.py
+++ test_requests.py
@@ -4,12 +4,16 @@ from datetime import datetime
 url = "http://localhost/fyp/insertdata.php"
 
 
-for i in range(5):
-    data = {'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        'temp': "22.6",
-        'humidity': "10.6",
-        'jerk': "4.19"}
-    print("sending at "+data['time'])
+for i in range(1):
+    data = {'datetime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'latitude': "10.009",
+        'longitude': "11.022",
+        'temperature': "32.6",
+        'humidity': "12.6",
+        'acceleration_x': "0",
+        'acceleration_y': "1",
+        'acceleration_z': "9.81"}
+    print("sending at "+data['datetime'])
     req = requests.post(url=url,data=data)
     time.sleep(1)
     print("return : %s\n"%req.text)
